306970e698307a7e69e1f02f18dd5a16a14f510f
WalidLabidi001 <walid.labidi001@gmail.com>
2025-09-14 08:42:11 -0400

    update message V2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello Flask depuis GitHub + OpenShift 🚀</h1>"
+    return "<h1>Bienvenue Walid – Flask v2 🎉</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
